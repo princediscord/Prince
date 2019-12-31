b43f9af71d04ea8ab3b00a023bf21238de8bdac2
princediscord <51452946+princediscord@users.noreply.github.com>
2019-12-31 09:29:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -192,9 +192,6 @@ client.on('message', message => {
          if(!message.channel.guild) return;
      let embed = new Discord.RichEmbed()
    .setColor("#8650a7")
-  .addField(message.author.username,message.author.avatarURL) 
-  .addField(message.author.username,message.author.avatarURL) 
-
   .addField("** :white_check_mark: Servers: **" , client.guilds.size)
   .addField("** :white_check_mark: Users: **" , client.users.size)
   .addField("** :white_check_mark: Channels: **" , client.channels.size)
